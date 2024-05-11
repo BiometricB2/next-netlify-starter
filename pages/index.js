f265c5985b38cff6bce531a9cec106caad7c1a57
BiometricB2 <151787244+BiometricB2@users.noreply.github.com>
2024-05-10 17:21:37 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Muffin & Duglass is watching you!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Games" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
